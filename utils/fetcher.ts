99af9f868434eec5102e62283e91d8d13f182bf3
Philippe <Philimart21@gmail.com>
2021-09-09 13:50:55 +0100

    update

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export const fetcher = () => {
+
   axios.interceptors.request.use((req: AxiosRequestConfig) => {
     console.log(req);
     return req;
