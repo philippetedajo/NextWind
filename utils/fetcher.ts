cc8b32321a12772f0c594b3bc63cb2b4bb291ed8
Philippe <Philimart21@gmail.com>
2021-09-10 09:31:01 +0100

    update

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,13 +1,37 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { Method } from "axios";
 
-export const fetcher = () => {
-  axios.interceptors.request.use((req: AxiosRequestConfig) => {
-    console.log(req);
-    return req;
-  });
+interface fetcherProps {
+  url: string;
+  method?: Method;
+  input?: object;
+  token?: string;
+}
 
-  axios.interceptors.response.use((res: AxiosResponse) => {
-    console.log(res);
-    return res;
-  });
+//normalize the response if necessary
+const responder = (type = false, message = "", data = null) => {
+  return { type: type, message: message, data: data };
+};
+
+//get data from api
+const fetcher = async (options: fetcherProps) => {
+  let header = { "Content-Type": "application/json" };
+  if (options.token) header["Authorization"] = options.token;
+
+  // for debugging input:
+  // console.log(options.input)
+
+  try {
+    let response = await axios({
+      url: options.url,
+      method: options.method,
+      data: options.input,
+      headers: header,
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log("API Not functional");
+    throw new Error(error);
+  }
 };
